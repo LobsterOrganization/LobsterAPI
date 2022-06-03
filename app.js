16276aa519e0d7e1922ed3b87c0c48fe4208db95
RKToni <45230098+RKToni@users.noreply.github.com>
2022-06-03 16:44:46 +0200

    Implementation security (#2)

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var helmet = require("helmet");
+var helmetCsp = require("helmet-csp");
+
+var cors = require("cors");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +26,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Style directory
+app.use(
+  "/stylesheets",
+  express.static(path.join(__dirname, "public/stylesheets"))
+);
+
+// js directory 
+app.use(
+  "/javascripts",
+  express.static(path.join(__dirname, "public/javascripts"))
+);
+
+// node_modules directory
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+
+// Helmet and Helmet-CSP
+app.use(helmet.xssFilter());
+app.use(helmet.frameguard());
+app.use(
+  helmetCsp({
+    useDefaults: true,
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+      ],
+      objectSrc: ["http://localhost:443/"],
+      upgradeInsecureRequests: [],
+    },
+    reportOnly: false, // Envoi un rapport
+  })
+);
+
+// Cors
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    méthodes: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
