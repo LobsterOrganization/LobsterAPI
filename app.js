bdecb7ee13828f67734d228917d14c440ede2633
RKToni <45230098+RKToni@users.noreply.github.com>
2022-06-17 12:03:05 +0200

    Add user schema (#4)

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 require("./services/databaseConnectionService");
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 var helmet = require("helmet");
 var helmetCsp = require("helmet-csp");
 
 var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users.router");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/", usersRouter);
 
 // Style directory
 app.use(
@@ -35,7 +35,7 @@ app.use(
   express.static(path.join(__dirname, "public/stylesheets"))
 );
 
-// js directory 
+// js directory
 app.use(
   "/javascripts",
   express.static(path.join(__dirname, "public/javascripts"))
@@ -52,9 +52,7 @@ app.use(
     useDefaults: true,
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: [
-        "'self'",
-      ],
+      scriptSrc: ["'self'", "http://localhost:3000"],
       objectSrc: ["http://localhost:443/"],
       upgradeInsecureRequests: [],
     },
@@ -64,26 +62,24 @@ app.use(
 
 // Cors
 app.use(
-  cors({
-    origin: "https://localhost:3000",
-    méthodes: ["GET", "POST", "PUT", "DELETE"],
-  })
+  cors()
 );
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
